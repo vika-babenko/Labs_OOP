4ddabe79466c752e1ce552d00bb577cb676bf7bc
vika-babenko <62217114+vika-babenko@users.noreply.github.com>
2020-05-31 16:45:44 +0300

    upgrade Main

--- Lab5_Babenko_oop/Lab5_Babenko_oop/Main.java
+++ Lab5_Babenko_oop/Lab5_Babenko_oop/Main.java
@@ -1,62 +1,84 @@
 package Lab5_Babenko_oop;
 
 public class Main {
+
+    //статичні методи
+    private static void myMethod(){
+        System.out.println("In my method");
+
+    }
+
+    //перевантаження
+    private static void myMethod(int b){
+        System.out.println("In my method(int b = " + b + ')');
+    }
+
+
     public static void main(String[] args){
+
+        Text text2 = new Text("Hello, world! Hello, Victoria!");
+        System.out.println(text2);
+
+        /**
+         * доступ клієнта до тексту
+         */
+        // !!! CLIENT
+        Client client = new Client(text2);
+        client.startLiveInTheSystem();
+
+        //myMethod(1);
+        //myMethod();
+
+/*
 /**
  * створення літер
  */
-        Letter letter = new Letter( 'A');
-        System.out.println(letter);
+        /*Letter letter = new Letter( 'A');
+                System.out.println(letter);
 
-        Letter letter2 = new Letter( 'a');
-        System.out.println(letter2);
+                Letter letter2 = new Letter( 'a');
+                System.out.println(letter2);
 
-        Letter letter3 = new Letter( 'B');
-        System.out.println(letter3);
-
-
-
-        //creating array of symbols
-        //quick creating of array
-        Letter [] letters = {
-                new Letter('H'),
-                new Letter('e'),
-                new Letter('l'),
-                new Letter('l'),
-                new Letter('o'),
-        };
+                Letter letter3 = new Letter( 'B');
+                System.out.println(letter3);
 
 
-        //передача масиву letters
-        Word word = new Word(letters);
+
+              //creating array of symbols
+                //quick creating of array
+               Letter [] letters = {
+                       new Letter('H'),
+                       new Letter('e'),
+                       new Letter('l'),
+                        new Letter('l'),
+                        new Letter('o'),
+                };
 
 
-        /**
-         * Створення слів
-         */
-        //quick creating of array
-        Letter [] letters2 = {
-                new Letter('w'),
-                new Letter('o'),
-                new Letter('r'),
-                new Letter('l'),
-                new Letter('d'),
-        };
+               //передача масиву letters
+                Word word = new Word(letters);
 
-        Word word2 = new Word(letters2);
-        System.out.println(word);
-        System.out.println(word2);
+
+                //quick creating of array
+                Letter [] letters2 = {
+                        new Letter('w'),
+                       new Letter('o'),
+                     new Letter('r'),
+                       new Letter('l'),
+                        new Letter('d'),
+                };
+
+               Word word2 = new Word(letters2);
+               System.out.println(word);
+               System.out.println(word2);
         //punctuation
 
-        /**
-         * передача пунктуації
+        /*
+          передача пунктуації
          */
-        Punctuation punctuation = new Punctuation(",");
+        /*Punctuation punctuation = new Punctuation(",");
         Punctuation punctuation2 = new Punctuation("!");
 
-        /**
-         * створення речення
-         */
         //creating of sentence as array
         Sentence sentence = new Sentence(new SentenceElement[]{
                 word,
@@ -93,20 +115,14 @@ public class Main {
 
 
 
-        Text text = new Text(new Sentence[]{sentence, sentence2});
+        Text text = new Text(new Sentence[]{sentence, sentence2});*/
+       // Text text2 = new Text("Hello, world. Hello, Viktoria.");
 
-        System.out.println(text);
+        //System.out.println(text);
 
 
-        /**
-         * доступ клієнта до тексту
-         */
-        // !!! CLIENT
-        Client client = new Client(text);
-        client.startLiveInTheSystem();
+
 
         System.out.println("Зроблено! :)");
-
-
     }
 }
