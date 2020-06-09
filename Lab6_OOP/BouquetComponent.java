9a0503067624376deaecd4141f40d9066ada01c4
vika-babenko <62217114+vika-babenko@users.noreply.github.com>
2020-06-09 10:31:22 +0300

    пошук квітки по довжині

--- Lab6_OOP/BouquetComponent.java
+++ Lab6_OOP/BouquetComponent.java
@@ -2,9 +2,32 @@ package Lab6_OOP;
 
 public class BouquetComponent {
     private int price;
+    private int length;
 
+
+    public BouquetComponent(int price, int length) {
+        this.price = price;
+        this.length = length;
+    }
 
     public BouquetComponent(int price) {
-        this.price = price;
+
     }
+
+    /**
+     * геттер для отримання довжини квітки
+     * потрібен для пошуку квітів в заданому діапазоні
+     */
+
+    public int getLength() {
+        return length;
+    }
+
+    // геттер для получения цены цветка, понадобится для поиска цветов в заданном диапазоне
+    public int getPrice() {
+        return price;
+    }
+
 }
+
+
