36d6f2ad64e3238d8c5278488e90ba9a8bb84531
vika-babenko <62217114+vika-babenko@users.noreply.github.com>
2020-06-09 10:33:56 +0300

    змінено toString та кконструктори

--- Lab6_OOP/Flower.java
+++ Lab6_OOP/Flower.java
@@ -1,12 +1,29 @@
 package Lab6_OOP;
 
+import java.util.Objects;
+
 public class Flower extends BouquetComponent{
     private int freshness;
+    private int length;
+    //generation of equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flower flower = (Flower) o;
+        return freshness == flower.freshness;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(freshness);
+    }
 
     //generation of constructor
-    public Flower(int price, int freshness) {
+    public Flower(int price, int freshness, int length) {
         super(price);
         this.freshness = freshness;
+        this.length = length;
     }
 
 //generation getter for freshness
@@ -14,13 +31,19 @@ public class Flower extends BouquetComponent{
         return freshness;
     }
 
+    @Override
+    public int getLength() {
+        return length;
+    }
+
     //generation toString for output in console
     //беремо клас об*єкту і отримуємо назву
     @Override
     public String toString() {
         return "Flower {" +
-                "class=" + /*this.*/getClass().getSimpleName() +
+                "name of flower=" + /*this.*/getClass().getSimpleName() +
                 ", freshness=" + freshness +
+                ", length=" + length +
                 '}';
     }
 
